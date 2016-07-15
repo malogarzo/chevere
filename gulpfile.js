ad8429a417cbf0d6d40e764e9c73adc1619aa6e8
Mariano Logarzo <malogarzo@Marianos-MacBook-Pro.local>
2016-07-15 01:51:30 -0300

    build automat & yo-yo

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,49 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task('styles', function () {
-	gulp
-		.src('index.scss')
-		.pipe(sass())
-		.pipe(rename('app.css'))
-		.pipe(gulp.dest('public'));
+  gulp
+    .src('index.scss')
+    .pipe(sass())
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('public'));
 })
 
 gulp.task('assets', function () {
-	gulp
-		.src('assets/*')
-		.pipe(gulp.dest('public'));
+  gulp
+    .src('assets/*')
+    .pipe(gulp.dest('public'));
 })
 
-gulp.task('scripts', function () {
-	browserify('./src/index.js')
-		.transform(babel)
-		.bundle()
-		.pipe(source('index.js'))
-		.pipe(rename('app.js'))
-		.pipe(gulp.dest('public'));
-})
+function compile(watch) {
+  var bundle = watchify(browserify('./src/index.js', {debug: true}));
 
-gulp.task('default', ['styles', 'assets', 'scripts'])
+  function rebundle() {
+    bundle
+      .transform(babel)
+      .bundle()
+      .pipe(source('index.js'))
+      .pipe(rename('app.js'))
+      .pipe(gulp.dest('public'));
+  }
+
+  if (watch) {
+    bundle.on('update', function () {
+      console.log('--> Bundling...');
+      rebundle();
+    });
+  }
+  rebundle();
+}
+
+gulp.task('build', function () {
+  return compile();
+});
+
+gulp.task('watch', function () {
+  return compile(true);
+});
+
+gulp.task('default', ['styles', 'assets', 'build']);
